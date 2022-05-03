1c4f69dd9c32c2e88fee28b0192fe8b1fd3a32e0
Mireia Alibau <malibasa7@alumnes.ub.edu>
2022-05-03 13:04:10 +0200

    Ioc updated

---
+++ Core/Src/components/chrono_amperometry.c
@@ -0,0 +1,11 @@
+/**
+  ******************************************************************************
+  * @file		chronoamperometry.c
+  * @brief		Gestión de la cronoamperometría.
+  * @author		Mireia Alibau, Sergio Venteo
+  * @copyright	Copyright 2022 Mireia Alibau, Sergio Venteo. All rights reserved.
+  ******************************************************************************
+  */
+
+#include "components/masb_comm_s.h"
+
