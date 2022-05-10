6f7e7668c985b71eacf4aa6bdcd0c6ae47bedf0b
sergio-venteo <sergio.venteo2222@gmail.com>
2022-05-10 17:31:11 +0200

    Modification source

--- Core/Src/components/adc.c
+++ Core/Src/components/adc.c
@@ -13,12 +13,12 @@ double ADCi;
 void ADC_v(void){
 	HAL_ADC_Start(&hadc1);
 	HAL_ADC_PollForConversion(&hadc1, 200);
-	ADCv = HAL_ADC_GetValue(&hadc1);
+	return HAL_ADC_GetValue(&hadc1);
 }
 
 void ADC_i(void){
 	HAL_ADC_Start(&hadc1);
 	HAL_ADC_PollForConversion(&hadc1, 200);
-	ADCi = HAL_ADC_GetValue(&hadc1);
+	return HAL_ADC_GetValue(&hadc1);
 }
 
